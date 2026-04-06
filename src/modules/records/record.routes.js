b14d13791c4ed0e06edbc8dbd5593b730cb1c5aa
Milan <milan903575@gmail.com>
2026-04-06 13:46:20 +0530

    added validation middleware

--- src/modules/records/record.routes.js
+++ src/modules/records/record.routes.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import authorize from '../../middleware/authorization.middleware.js';
 import { ROLES } from '../../constants/roles.js'
-
 import recordController from './record.controller.js';
+import validator from '../../middleware/validator.middleware.js';
 
 const router = express.Router();
 
-router.get('/', authorize(ROLES.ANALYST, ROLES.ADMIN), recordController.getRecords);
+router.post('/', authorize(ROLES.ADMIN), validator.createRecord, recordController.createRecord);
 
-router.post('/', authorize(ROLES.ADMIN), recordController.createRecord);
+router.get('/', authorize(ROLES.ANALYST, ROLES.ADMIN), validator.getRecords, recordController.getRecords);
 
-router.get('/:id', authorize(ROLES.ANALYST, ROLES.ADMIN), recordController.getRecord);
+router.get('/:id', authorize(ROLES.ANALYST, ROLES.ADMIN), validator.validateId, recordController.getRecord);
 
-router.patch('/:id', authorize(ROLES.ADMIN), recordController.updateRecord);
+router.patch('/:id', authorize(ROLES.ADMIN), validator.updateRecord, recordController.updateRecord);
 
-router.delete('/:id', authorize(ROLES.ADMIN), recordController.deleteRecord);
+router.delete('/:id', authorize(ROLES.ADMIN), validator.validateId, recordController.deleteRecord);
 
 export default router;
--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import controller from './user.controller.js';
+import validator from '../../middleware/validator.middleware.js';
 const router = express.Router();
 
 router.get('/', controller.getUsers);
-router.post('/', controller.createUser);
-router.get('/:id', controller.getUser);
-router.patch('/:id/role/', controller.updateUserRole);
-router.patch('/:id/status/', controller.updateUserStatus);
+
+router.post('/', validator.createUser, controller.createUser);
+
+router.get('/:id', validator.validateId, controller.getUser);
+
+router.patch('/:id/role/', validator.updateUserRole, controller.updateUserRole);
+
+router.patch('/:id/status/', validator.updateUserStatus, controller.updateUserStatus);
 
 export default router;
